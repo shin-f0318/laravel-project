592199f3a3b7a16686ef83d35fd02514ae7e79aa
shin-f0318 <sf.0318ss@gmail.com>
2022-10-26 07:49:53 +0900

    laravel2

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\introductionController;
+use App\Http\Controllers\contactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +21,5 @@ Route::get('/', function () {
 
 Route::get('hello', [HelloController::class, 'index']);
 Route::get('introduction', [introductionController::class, 'introduction']);
+Route::get('contact', [contactController::class, 'contact']);
+Route::post('confirmation', [contactController::class, 'confirmation']);
