be8848e15e821646e9e2f449fcd8e7632777d16d
shin-f0318 <sf.0318ss@gmail.com>
2022-10-26 07:55:29 +0900

    laravel2

---
+++ app/Http/Controllers/contactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class contactController extends Controller
+{
+    public function contact(){ 
+        return view('contact');
+    }
+    public function confirmation(Request $request){
+        return view('confirmation');
+    }    
+    /* public function contact() { 
+        $contact_submit function ()= {
+        }
+        , compact('contact_submit'));
+    } */
+}
