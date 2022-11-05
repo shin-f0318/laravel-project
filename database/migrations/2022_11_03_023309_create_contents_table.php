42ccbe8e7be7b8ff7358fd35aad438194b9aff98
shin-f0318 <sf.0318ss@gmail.com>
2022-11-05 11:28:21 +0900

    fourteen

---
+++ database/migrations/2022_11_03_023309_create_contents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->string('name');
+            $table->string('hurigana');
+            $table->string('email');
+            $table->string('tel');
+            $table->text('message');
+            $table->string('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+};
